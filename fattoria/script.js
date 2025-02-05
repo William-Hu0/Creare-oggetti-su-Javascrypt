28cebf2c6cee3227aaa0ca04244ef4be9411a7f9
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-02-05 10:01:37 +0000

    finito

--- fattoria/script.js
+++ fattoria/script.js
@@ -1,9 +1,25 @@
-const mucca = {
-    zampe: 4,
-    colore : "bianca e nera",
-    razza:  "felina"
-  }
+const animale1 = {
+  specie: "mucca",
+  razza: "chianina",
+  zampe:4
+};
+const animale2 = {
+  specie: "maiale",
+  razza: "kunekune",
+  zampe:4
+};
+const animale3 = {
+  specie: "gallina",
+  razza: "andalusa",
+  zampe:2
+};
 
 function mucca(){
-    document.getElementById("mucca").innerHTML= `Zampe: ${mucca.zampe}, Colore: ${mucca.colore}, Razza: ${mucca.razza}`;
+    document.getElementById("mucca").innerHTML= `Specie: ${animale1.specie}, Razza: ${animale1.razza}, Zampe: ${animale1.zampe}`;
+}
+function maiale(){
+  document.getElementById("maiale").innerHTML= `Specie: ${animale2.specie}, Razza: ${animale2.razza}, Zampe: ${animale2.zampe}`;
+}
+function  gallo(){
+  document.getElementById("gallo").innerHTML= `Specie: ${animale3.specie}, Razza: ${animale3.razza}, Zampe: ${animale3.zampe}`;
 }
